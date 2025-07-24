815960d5b383613be1ba471b71fa76553e367dd7
Ghali <147619387+Ghali062@users.noreply.github.com>
2025-07-24 12:07:21 +0100

    Create download_model.py

---
+++ download_model.py
@@ -0,0 +1,13 @@
+
+import gdown
+import os
+
+MODEL_PATH = "clv_model_pipeline.pkl"
+GDRIVE_ID = "1McG3vrjueVBdl5daB_2vCAEjcSY6amVU" # ID du fichier
+
+if not os.path.exists(MODEL_PATH):
+    print("Downloading model from Google Drive...")
+    url = f"https://drive.google.com/uc?id={GDRIVE_ID}"
+    gdown.download(url, MODEL_PATH, quiet=False)
+else:
+    print("Model already exists.")
